3606dec02cd903f13b7e9d9ef17d9eea122fe333
rainman0o <kupismarcel07@gmail.com>
2025-06-13 22:34:29 +0200

    jakies zadanie

--- PythonApplication1/PythonTablice.py
+++ PythonApplication1/PythonTablice.py
@@ -1,3 +1,43 @@
-a = [1, 2, 3, 4, 5, 6]
-print(len(a))
-print(a[0])
+import random
+
+testList = []
+
+for i in range (1, 11, 1):
+    testList.append(random.randrange(1,100))
+
+print(testList)
+
+sortedTestList = sorted(testList)
+print(sortedTestList)
+
+
+randomLinksList = [
+    "https://www.instagram.com/",
+    "https://www.facebook.com/",
+    "https://www.youtube.com/",
+    "https://x.com/home"
+    ]
+
+print(randomLinksList)
+for link in randomLinksList:
+    print(link.removeprefix("https://"))
+
+howManyEvens = 0
+howManyOdd = 0
+for i in testList:
+    if i % 2 == 0:
+        howManyEvens += 1
+    elif i % 2 == 1:
+        howManyOdd += 1
+
+
+print(f"How much evens: {howManyEvens}")
+print(f"How much odds: {howManyOdd}")
+
+for x in range (0, len(testList)):
+    print(testList[x])
+
+print("\n")
+
+for y in testList:
+    print(y)
